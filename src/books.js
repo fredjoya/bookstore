776eddde9139aac7b6a182f08e730dacfaddd63b
Your Name <ferdousjoya>
2021-11-05 15:22:57 -0700

    add bookstore app

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+  
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51x4JBwke7L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "The Year of Dreaming Dangerously",
+    author: "Zizek"
+  },
+
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51sIPBiMS7L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Phenomenology of Spirit",
+    author: "Hegel"
+  }, 
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/417JWkzzrNL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Critique of Pure Reason",
+    author: "Kant"
+  }
+];  
